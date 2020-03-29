c2fd36881723412ad1723f44ef039fb1b3417633
JUAN <jjrj23@hotmail.com>
2020-03-29 00:24:14 -0600

    REST SERVER USUARIO

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // port
 //==========
 process.env.PORT = process.env.PORT || 3000;
+
+//==========
+// Entorno
+//==========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==========
+// DB
+//==========
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jruvalcaba:kJdi23yXbPAoJtOS@cluster0-ajlsr.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
-const express = require('express')
+
+const express = require('express');
+const mongoose = require('mongoose');
+// mongoose.set('useFindAndModify', false);
 const app = express()
 
 const bodyParser = require('body-parser');
@@ -9,38 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesarop'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
-
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, }, (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+//mongoose.connect('mongodb://localhost:27017/cafe',);
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto ${ process.env.PORT }`);
 })
